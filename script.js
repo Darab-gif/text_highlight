6e2b44f53d4343c18b682cc103f25bc6c961cecc
Darab <darabcode@gmail.com>
2022-05-01 07:05:08 +0100

    Updated the CSS media query

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-/*const firstInput = document.querySelector("#text1").value;
-const secondInput = document.querySelector("#text2").value;
-const header = document.querySelector("#header");*/
-
 function myApp() {
   var firstInput = document.querySelector(".firstInput").value;
   var secondInput = document.querySelector(".secondInput").value;
   var header = document.querySelector("#headerResult");
   var index = firstInput.search(secondInput);
-  console.log(index);
+  //console.log(index);
   if (index !== -1) {
     firstInput =
       firstInput.slice(0, index) +

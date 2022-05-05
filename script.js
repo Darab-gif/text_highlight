45cbecfdd89da1032ad35914ace21711cbfde37f
Darab <darabcode@gmail.com>
2022-05-05 18:16:48 +0100

    * Fixed all necessary scenerio *Updated the style.css

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function myApp() {
-  var firstInput = document.querySelector(".firstInput").value;
-  var secondInput = document.querySelector(".secondInput").value;
-  var header = document.querySelector("#headerResult");
-  var index = firstInput.search(secondInput);
-  //console.log(index);
-  if (index !== -1) {
-    firstInput =
-      firstInput.slice(0, index) +
-      `<span class='highlight'>  ${firstInput.slice(
-        index,
-        index + secondInput.length
-      )} </span>` +
-      firstInput.slice(index + secondInput.length);
-    header.innerHTML = firstInput;
-  } else {
-    alert("Cannot highlight word in the sentence");
+  const firstInput = document.querySelector(".firstInput").value;
+  const secondInput = document.querySelector(".secondInput").value;
+  const keywordRegex = new RegExp(secondInput, "gi");
+  const matchedKeywords = firstInput.match(keywordRegex);
+
+  console.log({ matchedKeywords });
+  if (!matchedKeywords) alert("Cannot highlight word in the sentence");
+  else if (matchedKeywords.length >= 3)
+    alert("Too many occurences to be highlighted");
+  else {
+    const highlightedWords = firstInput.replace(
+      keywordRegex,
+      `<span class='highlight'>${secondInput}</span>`
+    );
+    document.querySelector("#headerResult").innerHTML = highlightedWords;
   }
 }
 
 //The onkeyup attribute functionality
 function myChange() {
-  var inputChange = document.querySelector(".firstInput").value;
-  var headerEvent = document.getElementById("headerResult");
+  const inputChange = document.querySelector(".firstInput").value;
+  const headerEvent = document.getElementById("headerResult");
 
   headerEvent.innerHTML = inputChange;
 }
